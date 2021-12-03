9a043d96cee723dc370f7b602b8d75e53fd987b8
jordandjp <64615330+jordandjp@users.noreply.github.com>
2021-12-03 14:23:41 -0500

    Responsibilities for abstractions changed and brightness and saturation filters were added.

--- fimage/ctransformations.py
+++ fimage/ctransformations.py
@@ -4,12 +4,13 @@
 import abc
 import math
 
-from fimage.channels import Channels
+import numpy as np
+from fimage.image_array import ImageArray
 
 
 class CTransformation(abc.ABC):
     @abc.abstractmethod
-    def process(self, channels: Channels) -> None:
+    def process(self, image_array: ImageArray) -> None:
         pass
 
 
@@ -17,45 +18,75 @@ class Sepia(CTransformation):
     def __init__(self, adjust: int = 100) -> None:
         self.adjust = adjust / 100
 
-    def process(self, channels: Channels) -> None:
-        channels.R = (
-            channels.R * (1 - (0.607 * self.adjust))
-            + (channels.G * (0.769 * self.adjust))
-            + (channels.B * (0.189 * self.adjust))
+    def process(self, image_array: ImageArray) -> None:
+        image_array.R = (
+            image_array.R * (1 - (0.607 * self.adjust))
+            + (image_array.G * (0.769 * self.adjust))
+            + (image_array.B * (0.189 * self.adjust))
         )
-        channels.G = (
-            (channels.R * (0.349 * self.adjust))
-            + (channels.G * (1 - (0.314 * self.adjust)))
-            + (channels.B * (0.168 * self.adjust))
+        image_array.G = (
+            (image_array.R * (0.349 * self.adjust))
+            + (image_array.G * (1 - (0.314 * self.adjust)))
+            + (image_array.B * (0.168 * self.adjust))
         )
-        channels.B = (
-            (channels.R * (0.272 * self.adjust))
-            + (channels.G * (0.534 * self.adjust))
-            + (channels.B * (1 - (0.869 * self.adjust)))
+        image_array.B = (
+            (image_array.R * (0.272 * self.adjust))
+            + (image_array.G * (0.534 * self.adjust))
+            + (image_array.B * (1 - (0.869 * self.adjust)))
         )
-        channels.constrain_channels()
+        image_array.constrain_channels()
 
 
 class Contrast(CTransformation):
     def __init__(self, adjust: int = 100) -> None:
         self.adjust = math.pow((adjust + 100) / 100, 2)
 
-    def process(self, channels: Channels) -> None:
-        channels.R = channels.R / 255
-        channels.R -= 0.5
-        channels.R *= self.adjust
-        channels.R += 0.5
-        channels.R *= 255
+    def process(self, image_array: ImageArray) -> None:
+        image_array.R = image_array.R / 255
+        image_array.R -= 0.5
+        image_array.R *= self.adjust
+        image_array.R += 0.5
+        image_array.R *= 255
 
-        channels.G = channels.G / 255
-        channels.G -= 0.5
-        channels.G *= self.adjust
-        channels.G += 0.5
-        channels.G *= 255
+        image_array.G = image_array.G / 255
+        image_array.G -= 0.5
+        image_array.G *= self.adjust
+        image_array.G += 0.5
+        image_array.G *= 255
 
-        channels.B = channels.B / 255
-        channels.B -= 0.5
-        channels.B *= self.adjust
-        channels.B += 0.5
-        channels.B *= 255
-        channels.constrain_channels()
+        image_array.B = image_array.B / 255
+        image_array.B -= 0.5
+        image_array.B *= self.adjust
+        image_array.B += 0.5
+        image_array.B *= 255
+        image_array.constrain_channels()
+
+
+class Brightness(CTransformation):
+    def __init__(self, adjust: int = 0) -> None:
+        self.adjust = math.floor(255 * (adjust / 100))
+
+    def process(self, image_array: ImageArray) -> None:
+        image_array.R += self.adjust
+        image_array.G += self.adjust
+        image_array.B += self.adjust
+        image_array.constrain_channels()
+
+
+class Saturation(CTransformation):
+    def __init__(self, adjust: int = 0) -> None:
+        self.adjust = adjust * -0.01
+
+    def process(self, image_array: ImageArray) -> None:
+        ndarray = image_array.get_current()
+
+        max_array = ndarray.max(axis=2)
+        max_array = np.repeat(max_array, 3).reshape(ndarray.shape)
+
+        sat_array = np.where(
+            max_array != ndarray, ndarray + (max_array - ndarray) * self.adjust, ndarray
+        )
+        image_array.R = sat_array[..., 0]
+        image_array.G = sat_array[..., 1]
+        image_array.B = sat_array[..., 2]
+        image_array.constrain_channels()
---
+++ fimage/image_array.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class ImageArray:
+    def __init__(self, ndarray: np.ndarray) -> None:
+        self.original_array = ndarray
+        self.R = self.original_array[..., 0]
+        self.G = self.original_array[..., 1]
+        self.B = self.original_array[..., 2]
+
+    def constrain_channels(self) -> None:
+        new_array = np.array([self.R, self.G, self.B], dtype=np.int16)
+        np.clip(new_array, 0, 255, out=new_array)
+        self.R, self.G, self.B = new_array
+
+    def get_current(self) -> np.ndarray:
+        """Return the array with the original shape and updated RGB values."""
+        new_array = np.empty(self.original_array.shape, dtype=np.uint8)
+        new_array[..., 0] = self.R
+        new_array[..., 1] = self.G
+        new_array[..., 2] = self.B
+        return new_array
