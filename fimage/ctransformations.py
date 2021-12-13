85b1f893d7ba187144954cf2c96381f63da7cb0e
Juan Benitez <juanbenitezdev@gmail.com>
2021-12-13 16:09:06 -0500

    feat: add exposure and curves transformations

--- fimage/ctransformations.py
+++ fimage/ctransformations.py
@@ -3,7 +3,7 @@
 
 import abc
 import math
-from typing import Dict
+from typing import Dict, Tuple
 
 import numpy as np
 from fimage.image_array import ImageArray
@@ -106,7 +106,6 @@ class Gamma(CTransformation):
         self.adjust = adjust
 
     def process(self, image_array: ImageArray) -> None:
-        f = np.vectorize(self.f)
         image_array.R = ((image_array.R / 255) ** self.adjust) * 255
         image_array.G = ((image_array.G / 255) ** self.adjust) * 255
         image_array.B = ((image_array.B / 255) ** self.adjust) * 255
@@ -180,3 +179,84 @@ class Channels(CTransformation):
                 image_array.B = image_array.B + (255 - image_array.B) * abs(self.B)
 
         image_array.constrain_channels()
+
+
+class Curves(CTransformation):
+    def __init__(self, p0: Tuple, p1: Tuple, p2: Tuple, p3: Tuple) -> None:
+        self.p0 = p0
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.curve = self.calculate_bezier(1000)
+
+    def clip(self, x: int) -> int:
+        if x < 0:
+            return 0    
+
+        if x > 255:
+            return 255
+
+        return x
+
+    
+    def missing_values(self, curve: Dict) -> Dict:
+        if len(curve.keys()) < 256:
+            # Some values might be missed, we need to add them
+            right_value = None
+            left_value = None
+
+            for index in range(256):
+                if index not in curve.keys():
+                    left_value = curve.get(index - 1)
+                    for i in range(index, 256):
+                        if i in curve.keys():
+                            right_value = curve.get(i)
+                            break
+                    
+                    if left_value is not None and right_value is not None:
+                        curve[index] = round((left_value + right_value) / 2)
+                    
+                    if left_value is None:
+                        curve[index] = right_value
+
+                    if right_value is None:
+                        curve[index] = left_value
+                    
+        return curve
+
+
+    def calculate_bezier(self, granuality: int) -> Dict:
+        result = dict()
+        
+        for i in range(granuality):
+            t = i / granuality
+            x = (1 - t) ** 3 * self.p0[0] + 3 * (1 - t) ** 2 * t * self.p1[0] + 3 * (1 - t) * t ** 2 * self.p2[0] + t ** 3 * self.p3[0]
+            y = (1 - t) ** 3 * self.p0[1] + 3 * (1 - t) ** 2 * t * self.p1[1] + 3 * (1 - t) * t ** 2 * self.p2[1] + t ** 3 * self.p3[1]
+
+            result[round(x)] = round(self.clip(y))
+
+        result = self.missing_values(result)
+
+        return result
+    
+
+    def process(self, image_array: ImageArray) -> None:
+        # https://stackoverflow.com/a/16993364
+        ndarray = image_array.get_current()
+
+        u, inv = np.unique(ndarray, return_inverse=True)
+        ndarray = np.array([self.curve.get(x) for x in u])[inv].reshape(ndarray.shape)
+
+        image_array.R = ndarray[..., 0]
+        image_array.G = ndarray[..., 1]
+        image_array.B = ndarray[..., 2]
+
+
+class Exposure(CTransformation):
+    def __init__(self, adjust: int = 1) -> None:
+        self.adjust = abs(adjust) / 100
+        self.p1 = (0, self.adjust * 255)
+        self.p2 = (255 - (self.adjust * 255), 255)
+
+    def process(self, image_array: ImageArray) -> None:
+        Curves((0, 0), self.p1, self.p2, (255, 255)).process(image_array)
